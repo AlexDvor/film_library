93b609e8ddc2fdb71ffcef2141f2ca65a5c8a021
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-08-08 17:29:45 +0200

    added refs js

--- src/js/changes-header.js
+++ src/js/changes-header.js
@@ -1,26 +1,21 @@
+import Refs from './refs'
 
-const linkMyLibraryRef = document.querySelector('.js-link-library');
-const linkMyHomeRef = document.querySelector('.js-link-home');
-const headerBackgroundImageRef = document.querySelector('.header-background-img');
-const searchFormRef = document.querySelector('.search-form');
-const headerButtonsRef = document.querySelector('.header-menu-btn');
-
-linkMyLibraryRef.addEventListener('click', onClickLib);
-linkMyHomeRef.addEventListener('click', onClickHome);
+Refs.linkMyLibraryRef.addEventListener('click', onClickLib);
+Refs.linkMyHomeRef.addEventListener('click', onClickHome);
 
 function onClickLib() {
-    searchFormRef.classList.add('is-closed');
-    headerButtonsRef.classList.replace('is-closed', 'is-open');
-    headerBackgroundImageRef.classList.replace('header-background-img', 'header-background-img-lib');
-    linkMyHomeRef.classList.remove('nav-menu__link--current-page');
-    linkMyLibraryRef.classList.add('nav-menu__link--current-page');
+    Refs.searchFormRef.classList.add('is-closed');
+    Refs.headerButtonsRef.classList.replace('is-closed', 'is-open');
+    Refs.headerBackgroundImageRef.classList.replace('header-background-img', 'header-background-img-lib');
+    Refs.linkMyHomeRef.classList.remove('nav-menu__link--current-page');
+    Refs.linkMyLibraryRef.classList.add('nav-menu__link--current-page');
 }
 
 function onClickHome() {
-    searchFormRef.classList.remove('is-closed');
-    headerButtonsRef.classList.replace('is-open', 'is-closed');
-    headerBackgroundImageRef.classList.replace('header-background-img-lib', 'header-background-img')
-    linkMyLibraryRef.classList.remove('nav-menu__link--current-page');
-    linkMyHomeRef.classList.add('nav-menu__link--current-page');
+    Refs.searchFormRef.classList.remove('is-closed');
+    Refs.headerButtonsRef.classList.replace('is-open', 'is-closed');
+    Refs.headerBackgroundImageRef.classList.replace('header-background-img-lib', 'header-background-img')
+    Refs.linkMyLibraryRef.classList.remove('nav-menu__link--current-page');
+    Refs.linkMyHomeRef.classList.add('nav-menu__link--current-page');
 }
 
---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+
+export default {
+
+linkMyLibraryRef: document.querySelector('.js-link-library'),
+linkMyHomeRef: document.querySelector('.js-link-home'),
+headerBackgroundImageRef: document.querySelector('.header-background-img'),
+searchFormRef: document.querySelector('.search-form'),
+headerButtonsRef: document.querySelector('.header-menu-btn'),
+
+}
